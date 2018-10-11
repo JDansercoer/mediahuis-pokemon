a8e0df39f71425415ef72c347215d930cca5c428
Jasper Dansercoer <jasper@kayzr.com>
2018-10-11 14:10:30 +0200

    ApiFetcher now refetches when the url updates

--- src/utils/ApiFetcher.js
+++ src/utils/ApiFetcher.js
@@ -12,6 +12,18 @@ class ApiFetcher extends React.Component {
   };
 
   componentWillMount = () => {
+    this.fetchData();
+  };
+
+  componentDidUpdate = ({ url: previousUrl }) => {
+    const { url } = this.props;
+
+    if (url !== previousUrl) {
+      this.fetchData();
+    }
+  };
+
+  fetchData = () => {
     const { url, field } = this.props;
 
     fetch(`https://pokeapi.co/api/v2/${url}`)
