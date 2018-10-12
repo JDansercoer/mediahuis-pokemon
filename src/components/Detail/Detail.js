99c46861876a57d2cca74778b3ad6eaa1a9c1cbd
Jasper Dansercoer <jasper@kayzr.com>
2018-10-12 11:01:40 +0200

    You can now see details and select/unselect moves

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,18 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import ApiFetcher from '../../utils/ApiFetcher';
 
 class Detail extends React.Component {
-  constructor() {
-    super();
-  }
+  state = {
+    selectedMoves: [],
+  };
+
+  selectMove = move => {
+    const { selectedMoves } = this.state;
+
+    if (_.find(selectedMoves, ['move.name', move.move.name])) {
+      this.setState(state => {
+        return {
+          selectedMoves: _.differenceBy(state.selectedMoves, [move], 'move.name'),
+        };
+      });
+    } else {
+      this.setState(state => {
+        return {
+          selectedMoves: _.concat(state.selectedMoves, [move]),
+        };
+      });
+    }
+  };
 
   render() {
     const { pokemonName } = this.props;
 
     return (
-      <ApiFetcher url={`pokemon/${pokemonName}/`} fields={['sprites', 'moves']}>
-        {({ sprites, moves }) => <img src={sprites.front_default} />}
+      <ApiFetcher url={`pokemon/${pokemonName}/`} fields={['sprites', 'moves', 'stats']}>
+        {({ sprites, moves, stats }) => {
+          const groupedMoves = _.groupBy(moves, move => {
+            return move.version_group_details[0].move_learn_method.name;
+          });
+          return (
+            <>
+              <img src={sprites.front_default} />
+              {_.map(this.state.selectedMoves, 'move.name')}
+              {_.map(stats, stat => (
+                <div key={stat.stat.name}>
+                  <strong>{stat.stat.name}</strong>
+                  {stat.base_stat}
+                </div>
+              ))}
+              {_.map(groupedMoves, (moveGroup, groupName) => {
+                return (
+                  <div key={groupName}>
+                    <strong>{groupName}</strong>
+                    {_.map(_.sortBy(moveGroup, ['move.name']), move => {
+                      return (
+                        <div key={move.move.name} onClick={() => this.selectMove(move)}>
+                          {move.move.name}
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
+              })}
+            </>
+          );
+        }}
       </ApiFetcher>
     );
   }
